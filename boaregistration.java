c226f4251f29c6b9f6a7e93f04379d0030fc3a66
eyuel14 <eyuel14@vt.edu>
2018-05-11 21:27:21 -0400

    New class added

---
+++ boaregistration.java
@@ -0,0 +1,14 @@
+
+public class boaregistration {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Ye3kebatlij\\Desktop\\SIS course\\Software\\driver.exe");
+		WebDriver driver = new ChromeDriver ();
+		
+		// This is GitHub example
+		
+	}
+
+}
